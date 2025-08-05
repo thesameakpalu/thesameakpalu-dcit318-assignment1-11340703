399cec8b7f2eb0d36ee62c293e6e991f30ad67b7
pamelaewenam <braemmanuel89@gmail.com>
2025-08-05 11:43:16 +0000

    My Triangle Type Identifier

--- GradeCalculator/obj/Debug/net8.0/GradeCalculator.AssemblyInfo.cs
+++ GradeCalculator/obj/Debug/net8.0/GradeCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6699868f0b056c10940fca96857f22b6c396545f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d15287795e63dde898fa8a89fbab657257c7e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueOption;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("=== Triangle Type Identifier ===");
+
+                Console.Write("Enter Side 1: ");
+                double side1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Side 2: ");
+                double side2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Side 3: ");
+                double side3 = Convert.ToDouble(Console.ReadLine());
+
+                // Check for triangle validity
+                if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+                {
+                    if (side1 == side2 && side2 == side3)
+                    {
+                        Console.WriteLine("Result: Equilateral Triangle");
+                    }
+                    else if (side1 == side2 || side1 == side3 || side2 == side3)
+                    {
+                        Console.WriteLine("Result: Isosceles Triangle");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Result: Scalene Triangle");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The values entered do not form a valid triangle.");
+                }
+
+                Console.Write("\nDo you want to check another triangle? (y/n): ");
+                continueOption = Console.ReadLine().ToLower();
+
+            } while (continueOption == "y" || continueOption == "yes");
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
